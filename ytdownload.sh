e52b4b079e155327ef34118f4be84bec60a14213
bob omen 2 <jsbachvu@gmail.com>
2024-03-08 13:45:17 +0100

    add album

--- ytdownload.sh
+++ ytdownload.sh
@@ -1,7 +1,7 @@
 while IFS="" read -r p || [ -n "$p" ]
 do
 	printf 'Downloading --- %s\n' "$p";
-	youtube-dl --ignore-errors -f bestaudio --extract-audio --audio-format mp3 --audio-quality 0 -o 'music/%(playlist)s/%(playlist_index)s-%(title)s.%(ext)s' "$p";
+	yt-dlp --ignore-errors -f bestaudio --extract-audio --audio-format mp3 --audio-quality 0 -o 'music/%(playlist)s/%(playlist_index)s-%(title)s.%(ext)s' "$p";
 done < list.txt
 
 >files.txt;
